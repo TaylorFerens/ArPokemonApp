04287a39eb3d05fd102785aadf458ef2d2a6a180
Taylor Ferens <tkferens@hotmail.com>
2020-08-20 18:41:41 -0500

    Initial commit

---
+++ app/src/main/java/com/squidster/arpokemonapp/AugmentedImageNode.kt
@@ -0,0 +1,68 @@
+package com.squidster.arpokemonapp
+
+import android.content.Context
+import android.widget.Toast
+import com.google.ar.core.AugmentedImage
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.animation.ModelAnimator
+import com.google.ar.sceneform.collision.Box
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import kotlin.math.max
+
+class AugmentedImageNode(
+    val context: Context
+): AnchorNode() {
+
+    var image: AugmentedImage? = null
+
+    private var modelCompletableFuture = ModelRenderable.builder()
+        .setSource(context, R.raw.beeddrill)
+        .build()
+
+    private  lateinit var renderable: ModelRenderable
+
+    fun setAugmentedImage(image: AugmentedImage){
+        this.image = image
+        // Check that the model is done rendering
+        if(!modelCompletableFuture.isDone){
+            modelCompletableFuture.thenAccept{
+                setAugmentedImage(image)
+            }.exceptionally {
+                Toast.makeText(context, "Error creating renderable", Toast.LENGTH_LONG).show()
+                null
+            }
+            return
+        }
+        renderable = modelCompletableFuture.get()
+        anchor = image.createAnchor(image.centerPose)
+
+        // AugmentedImageNode is the target image
+        val modelNode = Node().apply{
+            setParent(this@AugmentedImageNode)
+            renderable = this@AugmentedImageNode.renderable
+        }
+
+        // Now we need to scale the 3D renderable to the size of the target
+        val renderableBox = renderable.collisionShape as Box
+        val maxEdgeSize = max(renderableBox.size.x, renderableBox.size.z)
+        val maxImageEdge = max(image.extentX, image.extentZ)
+        val modelScale = (maxImageEdge / maxEdgeSize) / 2f
+
+        modelNode.localScale = Vector3(modelScale, modelScale, modelScale)
+
+        startAnimation()
+    }
+
+    private fun startAnimation(){
+        if(renderable.animationDataCount == 0){
+            return
+        }
+        val animationData = renderable.getAnimationData("Beedrill_Animation")
+        ModelAnimator(animationData, renderable).apply{
+            repeatCount = ModelAnimator.INFINITE
+            start()
+        }
+    }
+}
